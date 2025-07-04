4f88557d953d8e961588385c648a36a992c12f51
Stanisław Wiśniewski <s.wisniewski1101@gmail.com>
2025-07-04 11:19:35 +0200

    finish some config files helpers

--- src/config_files/mod.rs
+++ src/config_files/mod.rs
@@ -1,1 +1,8 @@
+use std::{env::home_dir, path::PathBuf};
+
 pub mod xdg;
+
+/// Wrapper around [`std::env::home_dir`](std::env::home_dir).
+pub fn home() -> Option<PathBuf> {
+    home_dir()
+}
--- src/config_files/xdg.rs
+++ src/config_files/xdg.rs
@@ -1,6 +1,8 @@
 use crate::os::env::Env;
 use std::path::PathBuf;
 
+use crate::config_files::home;
+
 /// How macOS XDG should be treated.
 #[derive(Debug, Clone, Copy, Hash)]
 pub enum MacOSBehaviour {
@@ -14,8 +16,131 @@ fn config_fallback(env: &Env, behaviour: MacOSBehaviour) -> Option<PathBuf> {
     if cfg!(target_os = "windows") {
         env.get("LOCALAPPDATA").ok().map(PathBuf::from)
     } else if cfg!(target_os = "macos") && matches!(behaviour, MacOSBehaviour::UseLibrary) {
-        None
+        home().map(|mut home| {
+            home.push("Library");
+            home.push("Application Support");
+            home
+        })
     } else {
-        None
+        home().map(|mut home| {
+            home.push(".config");
+            home
+        })
     }
+}
+
+fn cache_fallback(env: &Env, behaviour: MacOSBehaviour) -> Option<PathBuf> {
+    if cfg!(target_os = "windows") {
+        env.get("LOCALAPPDATA").ok().map(|localappdata| {
+            let mut buf = PathBuf::from(localappdata);
+            buf.push("caches");
+            buf
+        })
+    } else if cfg!(target_os = "macos") && matches!(behaviour, MacOSBehaviour::UseLibrary) {
+        home().map(|mut home| {
+            home.push("Library");
+            home.push("Caches");
+            home
+        })
+    } else {
+        home().map(|mut home| {
+            home.push(".cache");
+            home
+        })
+    }
+}
+
+fn data_fallback(env: &Env, behaviour: MacOSBehaviour) -> Option<PathBuf> {
+    if cfg!(target_os = "windows") {
+        env.get("LOCALAPPDATA").ok().map(PathBuf::from)
+    } else if cfg!(target_os = "macos") && matches!(behaviour, MacOSBehaviour::UseLibrary) {
+        home().map(|mut home| {
+            home.push("Library");
+            home.push("Application Support");
+            home
+        })
+    } else {
+        home().map(|mut home| {
+            home.push(".local");
+            home.push(".share");
+            home
+        })
+    }
+}
+
+fn state_fallback(env: &Env, behaviour: MacOSBehaviour) -> Option<PathBuf> {
+    if cfg!(target_os = "windows") {
+        env.get("LOCALAPPDATA").ok().map(PathBuf::from)
+    } else if cfg!(target_os = "macos") && matches!(behaviour, MacOSBehaviour::UseLibrary) {
+        home().map(|mut home| {
+            home.push("Library");
+            home.push("Application Support");
+            home
+        })
+    } else {
+        home().map(|mut home| {
+            home.push(".local");
+            home.push(".state");
+            home
+        })
+    }
+}
+
+///.Get proper path for `$XDG_CONFIG_HOME`.
+///
+/// # Returns
+///
+/// Most of time it should be `Some` variant.
+/// `None` is returned if and only if:
+///     1. [`rustvil::config_files::home`] returns `None`.
+///     2. `env` has no key `XDG_CONFIG_HOME`.
+pub fn config(env: &Env, behaviour: MacOSBehaviour) -> Option<PathBuf> {
+    env.get("XDG_CONFIG_HOME")
+        .ok()
+        .map(PathBuf::from)
+        .or_else(|| config_fallback(env, behaviour))
+}
+
+///.Get proper path for `$XDG_DATA_HOME`.
+///
+/// # Returns
+///
+/// Most of time it should be `Some` variant.
+/// `None` is returned if and only if:
+///     1. [`rustvil::config_files::home`] returns `None`.
+///     2. `env` has no key `XDG_DATA_HOME`.
+pub fn data(env: &Env, behaviour: MacOSBehaviour) -> Option<PathBuf> {
+    env.get("XDG_DATA_HOME")
+        .ok()
+        .map(PathBuf::from)
+        .or_else(|| data_fallback(env, behaviour))
+}
+
+///.Get proper path for `$XDG_CACHE_HOME`.
+///
+/// # Returns
+///
+/// Most of time it should be `Some` variant.
+/// `None` is returned if and only if:
+///     1. [`rustvil::config_files::home`] returns `None`.
+///     2. `env` has no key `XDG_CACHE_HOME`.
+pub fn cache(env: &Env, behaviour: MacOSBehaviour) -> Option<PathBuf> {
+    env.get("XDG_CACHE_HOME")
+        .ok()
+        .map(PathBuf::from)
+        .or_else(|| cache_fallback(env, behaviour))
+}
+///.Get proper path for `$XDG_STATE_HOME`.
+///
+/// # Returns
+///
+/// Most of time it should be `Some` variant.
+/// `None` is returned if and only if:
+///     1. [`rustvil::config_files::home`] returns `None`.
+///     2. `env` has no key `XDG_STATE_HOME`.
+pub fn state(env: &Env, behaviour: MacOSBehaviour) -> Option<PathBuf> {
+    env.get("XDG_STATE_HOME")
+        .ok()
+        .map(PathBuf::from)
+        .or_else(|| state_fallback(env, behaviour))
 }
